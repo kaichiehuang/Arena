b009da9cf8186ef74384a9ab60918e1f9a4894fd
kaichiehuang <kaichieh@andrew.cmu.edu>
2020-06-22 19:26:15 -0700

    update 3D model

--- test.py
+++ test.py
@@ -1,27 +1,19 @@
 import arena
 arena.init("oz.andrew.cmu.edu", "realm", "kaichieh1")
 basketballStand = arena.Object(objType=arena.Shape.gltf_model,
-    location=(0, 0, 0),
+    location=(0, -0.35, 0),
+    rotation=(0,0,0,0),
     scale=(0.01, 0.01, 0.01),
     url="https://rawcdn.githack.com/kaichiehuang/Arena/3ff149db669fd06ee05ed7b542faad1bd85a6ee8/3Dmodels/basketballStand.glb?raw=true")
 
-print('yo')
 
-# cybertruck = arena.Object(
-#     objName="cybertruck",
-#     objType=arena.Shape.gltf_model,
-#     location=(0, 3, 0.4),
-#     scale=(0.1, 0.1, 0.01),
-#     url="https://xr.andrew.cmu.edu/models/AnimatedTriangle.gltf"
-# )
-#cybertruck.update(data='{"animation-mixer": {"clip": "*"}}')
-# cow = arena.Object(
-#     objName="model2",
-#     objType=arena.Shape.gltf_model,
-#     location=(-21, 1.8, -8),
-#     scale=(0.02, 0.02, 0.02),
-#     url="https://xr.andrew.cmu.edu/models/cow2/scene.gltf",
-# )
+basketball = arena.Object(objType=arena.Shape.gltf_model,
+    scale=(0.1, 0.1, 0.1),
+    location=(0, 2, 0),
+    url="https://rawcdn.githack.com/kaichiehuang/Arena/908cc639306670e595a06b21e13d37c5cb66c29f/3Dmodels/basketball.glb?raw=true")
+
+basketball.update(physics=arena.Physics.dynamic)
+
 
 arena.handle_events()
     
